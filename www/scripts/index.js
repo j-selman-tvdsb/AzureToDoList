70f8892cd2829dff2679e63385e684300ff33d9f
j-selman-tvdsb <j.selman@tvdsb.on.ca>
2017-01-06 16:43:15 -0500

    jhli

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,27 +1,29 @@
 ﻿
 
 
-    //document.addEventListener("deviceready", onDeviceReady, false);
-	onDeviceReady();
+    document.addEventListener("deviceready", onDeviceReady, false);
+	//onDeviceReady();
+	
 
     function onDeviceReady() {
       
-           $.ajax({
-                url: "http://azurerestservice20170105011812.azurewebsites.net/api/ToDoItem"
-            }).then(function (data) {
-
-                $.each(data, function (index, item) {
-                    $("#ToDoItems").append("<input type='checkbox' id='chk" + item.id + "'/>" + item.Text + "<br/>");
-
-                    if (item.Complete === 1) {
-                        $('#chk' + item.id).attr('checked', true);
-
-                    }
-                });
-
-            });
+        $.ajax({
+        url: "http://azurerestservice20170105011812.azurewebsites.net/api/ToDoItem"
+    }).then(function(data) {
+       
+	   $.each(data, function(index,item) {        
+			$("#ToDoItems").append("<input type='checkbox' id='chk" + item.id + "'/>" + item.Text + "<br/>");
 			
-			alert("helloWorld");
+			if(item.Complete==1)
+			{
+				$('#chk' + item.id).attr('checked',true);
+				
+			}
+		});
+       
+    });
+			
+			
 			
 			
 			
