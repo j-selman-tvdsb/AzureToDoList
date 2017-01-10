2afc1588c00e3c0681f2c9c6027494d1fc340a77
j-selman-tvdsb <j.selman@tvdsb.on.ca>
2017-01-10 16:39:54 -0500

    changes to update add items

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,33 +1,93 @@
-﻿
-
-
-    ///document.addEventListener("deviceready", onDeviceReady, false);
-	onDeviceReady();
+﻿document.addEventListener("deviceready", onDeviceReady, false);
+	//onDeviceReady();
+	
 	
 
     function onDeviceReady() {
       
-        $.ajax({
+    	 
+	   showToDoList();
+	 
+	        
+    }
+	
+	function showToDoList()
+	{
+		$("#ToDoItems").html("");
+		
+		$.ajax({
         url: "http://azurerestservice20170105011812.azurewebsites.net/api/ToDoItem"
-    }).then(function(data) {
+      }).then(function(data) {
        
 	   $.each(data, function(index,item) {        
-			$("#ToDoItems").append("<input type='checkbox' id='chk" + item.id + "'/>" + item.Text + "<br/>");
-			
-			if(item.Complete==1)
-			{
-				$('#chk' + item.id).attr('checked',true);
-				
-			}
+			$("#ToDoItems").append("<input type='checkbox' class='chkbox' id='" + item.id + "' onchange='showID(this);'/>" + item.Text + "<br/>");
+					
 		});
-       
+				       
     });
-			
-			
-			
-			
-			
-        
-    }
+		
+	}
+	
+	function showID(thisCheckbox)
+	{
+		
+		if(thisCheckbox.checked)
+		{
+				markAsCompleted(thisCheckbox.id);
+		}
+		
+	}
 
+	
+	function addData() {
+		$("#txtToDoItem").prop('disabled',true);
+		
+        jQuery.support.cors = true;
+        var toDoItem = {
+             Text: $("#txtToDoItem").val()
+        };       
+        
+        $.ajax({
+            url: 'http://azurerestservice20170105011812.azurewebsites.net/api/ToDoItem',
+            type: 'POST',
+            data:JSON.stringify(toDoItem),            
+            contentType: "application/json;charset=utf-8",
+            success: function (data) {
+                alert(data);
+				showToDoList();
+				$("#txtToDoItem").val('');
+				$("#txtToDoItem").prop('disabled',false);
+            },
+            error: function (x, y, z) {
+                alert(x + '\n' + y + '\n' + z);
+            }
+        });
+    }
+	
+	
+	function markAsCompleted(id_num) {
+		
+		
+        jQuery.support.cors = true;
+        var toDoItem = {
+             id: id_num
+        };       
+        
+        $.ajax({
+            url: 'http://azurerestservice20170105011812.azurewebsites.net/api/ToDoItem',
+            type: 'PUT',
+            data:JSON.stringify(toDoItem),            
+            contentType: "application/json;charset=utf-8",
+            success: function (data) {
+                
+				showToDoList();
+				
+				
+            },
+            error: function (x, y, z) {
+                alert(x + '\n' + y + '\n' + z);
+            }
+        });
+    }
+	
    
